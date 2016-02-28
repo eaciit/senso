a1a98f3ca366b4c48f2486b81a71e0f8a9439a57
Arief Darmawan <ariefda@hotmail.com>
2016-02-29 06:45:33 +0700

    add coordinate

--- mapping.go
+++ mapping.go
@@ -7,15 +7,24 @@ type DocumentMapping struct {
 }
 
 type RecognitionMap struct {
+	Coordinate
+	ExpectedValue string
 }
 
 type FieldMap struct {
-	IsArray   bool
-	Start     []RecognitionMap
-	End       []RecognitionMap
-	SingleMap SingleMap
-	ListMap   []SingleMap
+	IsArray         bool
+	Start           []RecognitionMap
+	End             []RecognitionMap
+	SingleMap       SingleMap
+	ListHeaderField string
+	ListMap         []SingleMap
 }
 
 type SingleMap struct {
+	Coordinate
+	Field string
+}
+
+type Coordinate struct {
+	OffsetX, OffsetY, X, Y int
 }
